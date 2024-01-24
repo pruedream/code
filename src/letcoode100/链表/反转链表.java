7350094a5d70a7e526d221e2ccdaddacf0fadbe8
xhy <203152807@qq.com>
2024-01-24 23:18:06 +0800

    1,2,3

---
+++ src/letcoode100/链表/反转链表.java
@@ -0,0 +1,50 @@
+package letcoode100.链表;
+
+/**
+ * @author spike
+ * @version 1.0.0
+ *
+ * 206. 反转链表
+ *
+ * 掌握迭代法和递归法
+ */
+public class 反转链表 {
+
+
+    class Solution {
+        public ListNode reverseList(ListNode head) {
+              //递归
+
+
+
+            //迭代
+
+          /*  ListNode q1= null;
+            ListNode q2= head;
+            ListNode q3=head;
+
+            while (q2!=null){
+                q3 = q2.next;
+                q2.next = q1;
+                q1= q2;
+                q2= q3;
+            }
+
+            return q1;
+           */
+        }
+
+
+    }
+
+
+
+    public class ListNode{
+        int val;
+        ListNode next;
+
+    }
+
+
+
+}
