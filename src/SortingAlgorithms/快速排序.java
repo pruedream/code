fd5c1fd07c7800db4fbb60a369dc66eca54e1aa3
xhy <203152807@qq.com>
2024-02-03 21:36:48 +0800

    快速排序

---
+++ src/SortingAlgorithms/快速排序.java
@@ -0,0 +1,51 @@
+package SortingAlgorithms;
+
+/**
+ * @author spike
+ * @version 1.0.0
+ */
+public class 快速排序 {
+
+    public static void main(String[] args) {
+        int[] qw = {2,5,8,1,4,8,34,9,6,25,47,65};
+
+         sort(qw,0,11);
+
+         for (int i : qw){
+             System.out.println(i);
+         }
+
+    }
+
+    /**
+     *
+     * @param sd
+     * @param left
+     * @param right
+     *
+     * left 与 right就是边界
+     */
+    public static void sort(int sd[],int left,int right){
+           if(left>=right){
+             return;
+           }
+           int i=left,h=left;
+           while (h!=right){
+               if(sd[h]<=sd[right]){
+                   int as=sd[i];
+                   sd[i]=sd[h];
+                   sd[h]=as;
+                   i++;
+               }
+               h++;
+           }
+           int zx=sd[i];
+           sd[i]=sd[right];
+           sd[right] = zx;
+
+           sort(sd,left,i-1);
+           sort(sd,i+1,right);
+    }
+
+
+}
