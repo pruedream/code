b4ca850a1754f1846473df1804bae25d7beed9d5
xhy <203152807@qq.com>
2024-02-04 23:43:49 +0800

    合并两个有序链表

---
+++ src/CodeTop/Top40/top7.java
@@ -0,0 +1,61 @@
+package CodeTop.Top40;
+
+/**
+ * @author spike
+ * @version 1.0.0
+ * <p>
+ * 21. 合并两个有序链表
+ */
+public class top7 {
+
+    public static void main(String[] args) {
+
+    }
+
+    class Solution {
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+            ListNode head = new ListNode();
+            ListNode relhead = head;
+
+            while (list1 != null && list2 != null) {
+                if (list1.val >= list2.val) {
+                    head.next = new ListNode(list2.val);
+                    list2 = list2.next;
+                } else {
+                    head.next = new ListNode(list1.val);
+                    list1 = list1.next;
+                }
+                head = head.next;
+
+            }
+
+            if (list1 != null) {
+                head.next = list1;
+            }
+            if (list2 != null) {
+                head.next = list2;
+            }
+
+
+            return relhead.next;
+        }
+    }
+
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
