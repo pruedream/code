c58e4c425440086582a21827e678c7abb53fec26
xhy <203152807@qq.com>
2024-02-07 23:45:06 +0800

    局部反转链表

---
+++ src/CodeTop/Top40/top11.java
@@ -0,0 +1,45 @@
+package CodeTop.Top40;
+
+/**
+ * @author spike
+ * @version 1.0.0
+ *
+ *
+ *92. 反转链表 II
+ *
+ */
+public class top11 {
+    class Solution {
+        public ListNode reverseBetween(ListNode head, int left, int right) {
+            if(left==1){
+                return reverse(head,right);
+            }
+            // 移动到需要反转的位置,转换为反转链表前n
+            head.next = reverseBetween(head.next,left-1,right-1);
+
+            return head;
+        }
+
+        // 反转链表前n,起始索引为1
+        ListNode path = null;
+        public ListNode reverse(ListNode head,int n){
+            if(n==1){
+                path = head.next;
+                return head;
+            }
+            ListNode reverse = reverse(head.next, n - 1);
+            head.next.next = head;
+            head.next = path;
+            return reverse;
+        }
+
+
+
+    }
+
+
+    class ListNode{
+        int val;
+        ListNode next;
+    }
+}
