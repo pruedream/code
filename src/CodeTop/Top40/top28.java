e1404798ec2e1663f7a49c36bac8a8432223b41c
xhy <203152807@qq.com>
2024-02-21 20:30:28 +0800

    222

--- src/CodeTop/Top40/top28.java
+++ src/CodeTop/Top40/top28.java
@@ -11,15 +11,17 @@ import java.util.Arrays;
 public class top28 {
 
     class Solution {
-        public int findLengthOfLCIS(int[] nums) {
-            int[] dp = new int[nums.length];
+        public int lengthOfLIS(int[] nums) {
+            int [] dp = new int[nums.length];
             Arrays.fill(dp,1);
             int max = 1;
 
             for(int i=1;i<nums.length;i++){
-                if(nums[i] >nums[i-1]){
-                    dp[i] = dp[i-1]+1;
-                    max = max > dp[i] ? max:dp[i];
+                for(int j=0;j<i;j++){
+                    if(nums[i]>nums[j]){
+                        dp[i] = Math.max(dp[i],dp[j]+1);
+                        max = max>dp[i] ? max:dp[i];
+                    }
                 }
             }
 
