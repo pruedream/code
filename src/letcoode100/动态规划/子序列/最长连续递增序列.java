b2fbaf8e12a819ac0cdb229e292d76a4f721acaa
xhy <203152807@qq.com>
2024-02-21 20:30:00 +0800

    222

---
+++ src/letcoode100/动态规划/子序列/最长连续递增序列.java
@@ -0,0 +1,27 @@
+package letcoode100.动态规划.子序列;
+
+import java.util.Arrays;
+
+/**
+ * @author spike
+ * @version 1.0.0
+ */
+public class 最长连续递增序列 {
+
+    class Solution {
+        public int findLengthOfLCIS(int[] nums) {
+            int[] dp = new int[nums.length];
+            Arrays.fill(dp,1);
+            int max = 1;
+
+            for(int i=1;i<nums.length;i++){
+                if(nums[i] >nums[i-1]){
+                    dp[i] = dp[i-1]+1;
+                    max = max > dp[i] ? max:dp[i];
+                }
+            }
+
+            return max;
+        }
+    }
+}
--- src/letcoode100/动态规划/子序列/最长递增子序列.java
+++ src/letcoode100/动态规划/子序列/最长递增子序列.java
@@ -1,5 +1,7 @@
 package letcoode100.动态规划.子序列;
 
+import java.util.Arrays;
+
 /**
  * @author spike
  * @version 1.0.0
@@ -9,7 +11,20 @@ public class 最长递增子序列 {
 
     class Solution {
         public int lengthOfLIS(int[] nums) {
+            int [] dp = new int[nums.length];
+            Arrays.fill(dp,1);
+            int max = 1;
 
+            for(int i=1;i<nums.length;i++){
+                for(int j=0;j<i;j++){
+                    if(nums[i]>nums[j]){
+                        dp[i] = Math.max(dp[i],dp[j]+1);
+                        max = max>dp[i] ? max:dp[i];
+                    }
+                }
+            }
+
+            return max;
         }
     }
 }
