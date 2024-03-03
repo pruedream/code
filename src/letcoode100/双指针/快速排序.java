785bc362d8a00696af40e2b9a5c9b4d1d098b2e7
xhy <203152807@qq.com>
2024-03-03 23:32:58 +0800

    222

---
+++ src/letcoode100/双指针/快速排序.java
@@ -0,0 +1,31 @@
+package letcoode100.双指针;
+
+/**
+ * @author spike
+ * @version 1.0.0
+ */
+public class 快速排序 {
+
+    public static void sort(int sd[],int left,int right){
+        if(left>=right){
+            return;
+        }
+        //用两个指针一起从头开始， 寻找小于的值，然后两个指针交换
+        int i=left,h=left;
+        while (h!=right){
+            if(sd[h]<=sd[right]){
+                int as=sd[i];
+                sd[i]=sd[h];
+                sd[h]=as;
+                i++;
+            }
+            h++;
+        }
+        int zx=sd[i];
+        sd[i]=sd[right];
+        sd[right] = zx;
+
+        sort(sd,left,i-1 );
+        sort(sd,i+1,right );
+    }
+}
