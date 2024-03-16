289e0cc8f788a39bead9f61f4b22d3c880e53354
xhy <203152807@qq.com>
2024-03-16 12:18:51 +0800

    222

--- src/面试手撕/设计模式/单例模式.java
+++ src/面试手撕/设计模式/单例模式.java
@@ -5,4 +5,54 @@ package 面试手撕.设计模式;
  * @version 1.0.0
  */
 public class 单例模式 {
+
+
+    private  单例模式(){
+        System.out.println("创建对象");
+    }
+
+
+    /**
+     * 饿汉式
+     */
+
+   /* private  static final 单例模式 instance = new 单例模式();
+
+    public static   单例模式 getInstance(){
+        return instance;
+    }*/
+
+    /**
+     * 懒汉式
+     */
+
+   /* private static volatile 单例模式 instance;
+
+
+    public static 单例模式  getInstance() {
+       if(instance == null){
+         synchronized (单例模式.class){
+             if(instance == null){
+                 instance = new 单例模式();
+             }
+         }
+       }
+
+       return instance;
+    }
+*/
+
+    /**
+     * 懒汉模式推荐 内部类实现
+     */
+
+    private static  class  Context{
+        private static final  单例模式 instance = new 单例模式();
+    }
+
+    public static 单例模式 getInstance(){
+        return Context.instance ;
+    }
+
+
 }
